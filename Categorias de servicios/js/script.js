630a9524f4483d864bc50583f6315eb3f3d4e39f
AngelIvanOC <20233tn086@utez.edu.mx>
2024-11-18 21:43:40 -0600

    Cambios 18/11/24

--- Categorias de servicios/js/script.js
+++ Categorias de servicios/js/script.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Modals
-    $('#exampleModal').on('show.bs.modal', function (event) {
-      var button = $(event.relatedTarget);
-      var recipient = button.data('whatever');
-      var modal = $(this);
-      modal.find('.modal-title').text('New message to ' + recipient);
-      modal.find('.modal-body input').val(recipient);
-    });
+  // Modals
+  $('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var recipient = button.data('whatever');
+    var modal = $(this);
+    modal.find('.modal-title').text('New message to ' + recipient);
+    modal.find('.modal-body input').val(recipient);
   });
-  
+});
