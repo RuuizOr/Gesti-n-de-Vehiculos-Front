d65203f98470baa560991875151b85d7192e376b
josemanuel11sans <20233tn078@utez.edu.mx>
2024-11-19 22:27:22 -0600

    redireccion de test

---
+++ Login/js/mientras.js
@@ -0,0 +1,11 @@
+// Espera a que el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el botón
+    const btnIngresar = document.getElementById('btnIngresar');
+
+    // Agrega el evento click al botón
+    btnIngresar.addEventListener('click', function () {
+        // Redirige a la página deseada
+        window.location.href = '../../InicioAdmin/InicioAdmin.html';
+    });
+});
